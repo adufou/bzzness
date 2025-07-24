61796f997b47043433911c0d5097385d8e177e89
adufou <antoine.du.fou@gmail.com>
2025-07-24 18:33:18 +0200

    feat: bees use the game state

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -1,7 +1,36 @@
 extends Node
 
+################## |---------------[ SIGNALS ]---------------| ##################
+##### |------- UPGRADES -------| #####
+### ----- Bees ----- ###
+signal on_update_bees_lifetime_seconds(value: float)
+signal on_update_bees_pollen_capacity(value: int)
+signal on_update_bees_speed(value: float)
+
+##### |------- GOODS -------| #####
+### ----- Currencies ----- ###
 signal on_update_total_pollen(value: int)
 
+################## |---------------[ VALUES ]---------------| ##################
+##### |------- UPGRADES -------| #####
+### ----- Bees ----- ###
+var bees_lifetime_seconds: float = 10:
+	set(value):
+		bees_lifetime_seconds = value
+		on_update_bees_lifetime_seconds.emit(value)
+
+var bees_pollen_capacity: int = 3:
+	set(value):
+		bees_pollen_capacity = value
+		on_update_bees_pollen_capacity.emit(value)
+
+var bees_speed: float = 5.0:
+	set(value):
+		bees_speed = value
+		on_update_bees_speed.emit(value)
+
+##### |------- GOODS -------| #####
+### ----- Currencies ----- ###
 var total_pollen: int = 0:
 	set(value):
 		total_pollen = value
--- modules/bee/bee.gd
+++ modules/bee/bee.gd
@@ -5,18 +5,22 @@ signal on_request_flower(bee: Bee)
 signal on_request_hive_cells_spot(bee: Bee)
 signal on_deposit_pollen(pollen: int)
 
-const LIFETIME_SECONDS: float = 10
-const POLLEN_CAPACITY: int = 3
-const SPEED: float = 5.0
+var lifetime_seconds: float = GameState.bees_lifetime_seconds
+var pollen_capacity: int = GameState.bees_pollen_capacity
+var speed: float = GameState.bees_speed
 
 var aimed_flower: Flower
 var hive_cells_spot: Vector3
 var pollen_carried: int = 0
-var remaining_lifetime: float = LIFETIME_SECONDS
+var remaining_lifetime: float = lifetime_seconds
 
 func _ready() -> void:
 	on_request_flower.emit(self)
 	on_request_hive_cells_spot.emit(self)
+
+	GameState.on_update_bees_lifetime_seconds.connect(_set_lifetime_seconds)
+	GameState.on_update_bees_pollen_capacity.connect(_set_pollen_capacity)
+	GameState.on_update_bees_speed.connect(_set_speed)
 
 func _process(delta: float) -> void:
 	remaining_lifetime -= delta
@@ -24,7 +28,7 @@ func _process(delta: float) -> void:
 		queue_free()
 	
 	%LifetimeLabel3D.text = "%.2fs" % remaining_lifetime
-	%CarryLabel3D.text = str(pollen_carried) + "/" + str(POLLEN_CAPACITY)
+	%CarryLabel3D.text = str(pollen_carried) + "/" + str(pollen_capacity)
 	
 	if is_full():
 		if is_at_hive_cells_spot():
@@ -40,6 +44,16 @@ func _process(delta: float) -> void:
 		else:
 			move_to_flower(delta)
 
+func _set_lifetime_seconds(value: float) -> void:
+	lifetime_seconds = value
+
+func _set_pollen_capacity(value: int) -> void:
+	pollen_capacity = value
+
+func _set_speed(value: float) -> void:
+	speed = value
+
+
 func check_flower() -> bool:
 	if aimed_flower == null:
 		on_request_flower.emit(self)
@@ -52,12 +66,12 @@ func deposit_pollen() -> void:
 	pollen_carried = 0
 
 func harvest_pollen() -> void:
-	var available_pollen_carry: int = POLLEN_CAPACITY - pollen_carried
+	var available_pollen_carry: int = pollen_capacity - pollen_carried
 	var pollen_harvested: int = aimed_flower.harvest_pollen(available_pollen_carry)
 	pollen_carried += pollen_harvested
 
 func is_full() -> bool:
-	return pollen_carried >= POLLEN_CAPACITY
+	return pollen_carried >= pollen_capacity
 
 func is_at_flower() -> bool:
 	var distance_to_flower: Vector3 = aimed_flower.position - global_transform.origin
@@ -66,7 +80,7 @@ func is_at_flower() -> bool:
 
 func move_to_flower(delta: float) -> void:
 	var direction: Vector3 = (aimed_flower.position - global_transform.origin).normalized()
-	var movement: Vector3 = direction * SPEED * delta
+	var movement: Vector3 = direction * speed * delta
 
 	global_transform.origin += movement
 
@@ -77,6 +91,6 @@ func is_at_hive_cells_spot() -> bool:
 
 func move_to_hive_cells_spot(delta: float) -> void:
 	var direction: Vector3 = (hive_cells_spot - global_transform.origin).normalized()
-	var movement: Vector3 = direction * SPEED * delta
+	var movement: Vector3 = direction * speed * delta
 
 	global_transform.origin += movement
