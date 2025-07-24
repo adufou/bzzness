dd2dd04a538f4b17f5fca57f67fe3d1cc62cac14
adufou <antoine.du.fou@gmail.com>
2025-07-24 20:48:03 +0200

    feat: can upgrade updates

--- autoload/game_state.gd
+++ autoload/game_state.gd
@@ -2,6 +2,12 @@ extends Node
 
 ################## |---------------[ SIGNALS ]---------------| ##################
 ##### |------- UPGRADES -------| #####
+### ----- Tier 1 ----- ###
+signal on_update_tier_1_upgrade_level_bee_speed(value: int)
+signal on_update_tier_1_upgrade_level_bee_lifetime(value: int)
+signal on_update_tier_1_upgrade_level_bee_carry_capacity(value: int)
+
+##### |------- STATISTICS -------| #####
 ### ----- Bees ----- ###
 signal on_update_bees_lifetime_seconds(value: float)
 signal on_update_bees_pollen_capacity(value: int)
@@ -13,18 +19,35 @@ signal on_update_total_pollen(value: int)
 
 ################## |---------------[ VALUES ]---------------| ##################
 ##### |------- UPGRADES -------| #####
+### ----- Tier 1 ----- ###
+var tier_1_upgrade_level_bee_speed: int = 1:
+	set(value):
+		tier_1_upgrade_level_bee_speed = value
+		on_update_tier_1_upgrade_level_bee_speed.emit(value)
+
+var tier_1_upgrade_level_bee_lifetime: int = 1:
+	set(value):
+		tier_1_upgrade_level_bee_lifetime = value
+		on_update_tier_1_upgrade_level_bee_lifetime.emit(value)
+
+var tier_1_upgrade_level_bee_carry_capacity: int = 1:
+	set(value):
+		tier_1_upgrade_level_bee_carry_capacity = value
+		on_update_tier_1_upgrade_level_bee_carry_capacity.emit(value)
+
+##### |------- STATISTICS -------| #####
 ### ----- Bees ----- ###
 var bees_lifetime_seconds: float = 10:
 	set(value):
 		bees_lifetime_seconds = value
 		on_update_bees_lifetime_seconds.emit(value)
 
-var bees_pollen_capacity: int = 3:
+var bees_pollen_capacity: int = 1:
 	set(value):
 		bees_pollen_capacity = value
 		on_update_bees_pollen_capacity.emit(value)
 
-var bees_speed: float = 5.0:
+var bees_speed: float = 1.0:
 	set(value):
 		bees_speed = value
 		on_update_bees_speed.emit(value)
--- upgrades_panel.gd
+++ upgrades_panel.gd
@@ -2,6 +2,32 @@ extends Control
 
 signal on_close_upgrades_panel
 
+func _ready() -> void:
+	GameState.on_update_tier_1_upgrade_level_bee_carry_capacity.connect(update_tier_1_upgrade_level_bee_carry_capacity)
+	GameState.on_update_tier_1_upgrade_level_bee_lifetime.connect(update_tier_1_upgrade_level_bee_lifetime)
+	GameState.on_update_tier_1_upgrade_level_bee_speed.connect(update_tier_1_upgrade_level_bee_speed)
+
+	update_tier_1_upgrade_level_bee_carry_capacity(GameState.tier_1_upgrade_level_bee_carry_capacity)
+	update_tier_1_upgrade_level_bee_lifetime(GameState.tier_1_upgrade_level_bee_lifetime)
+	update_tier_1_upgrade_level_bee_speed(GameState.tier_1_upgrade_level_bee_speed)
+
+func update_tier_1_upgrade_level_bee_carry_capacity(new_level: int):
+	%Tier1BeeCarryLevelLabel.text = "Lv. " + str(new_level)
+
+func update_tier_1_upgrade_level_bee_lifetime(new_level: int):
+	%Tier1BeeLifetimeLevelLabel.text = "Lv. " + str(new_level)
+
+func update_tier_1_upgrade_level_bee_speed(new_level: int):
+	%Tier1BeeSpeedLevelLabel.text = "Lv. " + str(new_level)
 
 func _on_close_upgrades_panel_button_pressed() -> void:
 	on_close_upgrades_panel.emit()
+
+func _on_tier_1_bee_carry_capacity_button_pressed() -> void:
+	GameState.tier_1_upgrade_level_bee_carry_capacity += 1
+
+func _on_tier_1_bee_lifetime_button_pressed() -> void:
+	GameState.tier_1_upgrade_level_bee_lifetime += 1
+
+func _on_tier_1_bee_speed_button_pressed() -> void:
+	GameState.tier_1_upgrade_level_bee_speed += 1
