0f133d67b5152eed7be146c99b91dccffeda27c1
adufou <antoine.du.fou@gmail.com>
2025-07-30 17:17:18 +0200

    feat: update base and upgrade values

--- constants/statistics_constants.gd
+++ constants/statistics_constants.gd
@@ -3,16 +3,16 @@ class_name StatisticsConstants
 
 ### |------- Bees -------| ###
 const BASE_BEE_SPEED: float = 2.0
-const BASE_BEE_CARRY_CAPACITY: int = 1
-const BASE_BEE_LIFETIME_SECONDS: float = 1.0
+const BASE_BEE_CARRY_CAPACITY: float = 0.01
+const BASE_BEE_LIFETIME_SECONDS: float = 10.0
 
 ### |------- Eggs -------| ###
-const BASE_EGG_AUTO_SPAWN_RATE_PER_SECOND: float = 1.0
+const BASE_EGG_AUTO_SPAWN_RATE_PER_SECOND: float = 1.0 / 10.0
 
 ### |------- Flowers -------| ###
-const BASE_FLOWER_SPAWN_RATE_PER_SECOND: float = 1.0
+const BASE_FLOWER_SPAWN_RATE_PER_SECOND: float = 1.0 / 100.0
 
 ### |------- Honey factory -------| ###
-const BASE_HONEY_FACTORY_PRODUCTION_RATE_PER_SECOND: float = 1.0
-const BASE_HONEY_FACTORY_PRODUCTION_QUANTITY: int = 1
+const BASE_HONEY_FACTORY_PRODUCTION_RATE_PER_SECOND: float = 1.0 / 100.0
+const BASE_HONEY_FACTORY_PRODUCTION_QUANTITY: float = 1.0 / 10.0
 const BASE_HONEY_FACTORY_POLLEN_TO_HONEY_RATE: float = 1_000.0
