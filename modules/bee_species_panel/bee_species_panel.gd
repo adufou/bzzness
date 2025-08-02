04f12b993c3da7607c85adad1db321f1d4bd17c2
adufou <antoine.du.fou@gmail.com>
2025-08-02 16:14:49 +0200

    feat: improve bee species panel

--- modules/bee_species_panel/bee_species_panel.gd
+++ modules/bee_species_panel/bee_species_panel.gd
@@ -2,35 +2,48 @@ extends Control
 
 @export var species_item_scene: PackedScene
 
-var _species_items: Dictionary[BeeSpecies.SpeciesEnum, BeeSpeciesPanelItem] = {}
-var current_species_item: BeeSpeciesPanelItem = null
+var _bee_species_items: Dictionary[BeeSpecies.SpeciesEnum, BeeSpeciesPanelItem]
+var current_bee_species: BeeSpecies.SpeciesEnum
 
 signal on_close_species_panel
 
 func _ready() -> void:
-	instantiate_species_items()
-
-func instantiate_species_items() -> void:
-	for species_name: String in BeeSpecies.SpeciesEnum:
-		var species_item: BeeSpeciesPanelItem = species_item_scene.instantiate()
-		species_item.bee_species_enum = BeeSpecies.SpeciesEnum[species_name]
-		_species_items[BeeSpecies.SpeciesEnum[species_name]] = species_item
-		
+	_instantiate_bee_species_items()
 	select_bee_species(GameState.bee_species)
-
-func select_bee_species(bee_species: BeeSpecies.SpeciesEnum) -> void:
-	if current_species_item:
-		current_species_item.queue_free()
 	
-	var selected_item: BeeSpeciesPanelItem = _species_items[bee_species]
-	%SpeciesGalleryMarginContainer.add_child(selected_item)
-
+func _instantiate_bee_species_items() -> void:
+	for bee_species_name: String in BeeSpecies.SpeciesEnum:
+		var species_item: BeeSpeciesPanelItem = species_item_scene.instantiate()
+		species_item.bee_species_enum = BeeSpecies.SpeciesEnum[bee_species_name]
+		species_item.hide()
+		
+		_bee_species_items[BeeSpecies.SpeciesEnum[bee_species_name]] = species_item
+		%BeeSpeciesGalleryMarginContainer.add_child(species_item)
+	
+func select_bee_species(bee_species: BeeSpecies.SpeciesEnum) -> void:	
+	_bee_species_items[current_bee_species].hide()
+		
+	current_bee_species = bee_species	
+	_bee_species_items[current_bee_species].show()
+	
+	_update_navigation_buttons_visibility()
+	
+func _update_navigation_buttons_visibility() -> void:	
+	if (current_bee_species == 0):
+		%PreviousButton.hide()
+	else:
+		%PreviousButton.show()
+	
+	if (current_bee_species == BeeSpecies.SpeciesEnum.size() - 1):
+		%NextButton.hide()
+	else:
+		%NextButton.show()
+		
 func _on_close_button_pressed() -> void:
 	on_close_species_panel.emit()
 
 func _on_previous_button_pressed() -> void:
-	pass # Replace with function body.
-
+	select_bee_species(current_bee_species - 1)
 
 func _on_next_button_pressed() -> void:
-	pass # Replace with function body.
+	select_bee_species(current_bee_species + 1)
--- modules/bee_species_panel_item/bee_species_panel_item.gd
+++ modules/bee_species_panel_item/bee_species_panel_item.gd
@@ -1,4 +1,17 @@
-extends VBoxContainer
+extends MarginContainer
 class_name BeeSpeciesPanelItem
 
 var bee_species_enum: BeeSpecies.SpeciesEnum
+
+func _ready() -> void:
+	%NameLabel.text = BeeSpecies.get_display_name(bee_species_enum)
+	%DescriptionLabel.text = BeeSpecies.get_description(bee_species_enum)
+	%MultiplierLabel.text = _prettify_multiplier()
+	%BuyButton.text = _prettify_buy()
+	
+func _prettify_buy() -> String:
+	return "Buy\n" + "%0.2f" % BeeSpecies.get_unlock_cost(bee_species_enum) + " Honey"
+
+func _prettify_multiplier() -> String:
+	return "x" + str(BeeSpecies.get_multiplier(bee_species_enum))
+	
